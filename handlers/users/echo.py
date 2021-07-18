54596243a9869de8644a46260fc5570f7cb0fe29
Maxutka <81700552+Maxutka00@users.noreply.github.com>
2021-07-18 22:38:29 +0300

    Update echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -4,12 +4,6 @@ from aiogram.dispatcher import FSMContext
 from loader import dp
 
 
-# Эхо хендлер, куда летят текстовые сообщения без указанного состояния
-@dp.message_handler(state=None)
-async def bot_echo(message: types.Message):
-    await message.answer(f"Эхо без состояния."
-                         f"Сообщение:\n"
-                         f"{message.text}")
 
 
 # Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
