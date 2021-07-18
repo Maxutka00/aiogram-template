6ea61676e17da23afbf841ebac461b766bf4b774
Maxutka <81700552+Maxutka00@users.noreply.github.com>
2021-07-18 22:40:27 +0300

    Update db.py

--- utils/db_api/db.py
+++ utils/db_api/db.py
@@ -5,6 +5,7 @@ import sqlite3
 class ActionWithDB():
 	"""Управление базой данных"""
 	def __init__(self):
+		#this code is only for using with sqlite3
 		self.conn= sqlite3.connect('data.db')
 		self.cursor = self.conn.cursor()
 		
